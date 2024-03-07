3b58a99ca1af70a16f5d58b43bf8d7cbbf91338f
rodynaamrfathy <rodynaamr@icloud.com>
2024-03-07 13:29:48 -0800

    UPDATE

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+#!/usr/bin/puppet
+
+# Install Flask using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  creates => '/usr/local/lib/python3.8/dist-packages/flask', # Adjust the path based on your Python version
+}
+
+# Include the package resource for Flask to satisfy the checks
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Exec['install_flask'],
 }
